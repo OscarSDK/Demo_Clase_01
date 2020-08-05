2eeae9ce003a192db604f8115cd9ba6b20280439
user <user@DESKTOP-EOQAJOP>
2020-08-05 07:17:00 -0500

    Primer Cambio, inicio de variable x=2

--- ProyectoDemo/src/proyectodemo/ProyectoDemo.java
+++ ProyectoDemo/src/proyectodemo/ProyectoDemo.java
@@ -17,6 +17,7 @@ public class ProyectoDemo {
     public static void main(String[] args) {
         // TODO code application logic 
         int x=2;
+        System.out.println("2");
     }
     
 }
